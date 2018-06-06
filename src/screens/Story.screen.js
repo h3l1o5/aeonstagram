8f2c80cec62f3dc3a88590f80475029f219797e2
martysun25 <martysun25@gmail.com>
2018-06-07 03:00:11 +0800

    Story screen fade in animation

--- src/screens/Story.screen.js
+++ src/screens/Story.screen.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
-import { View, Text, Image, Button } from "react-native";
+import { View, Text, Image, Button, Animated } from "react-native";
 import firebase from "react-native-firebase";
 
 class StoryScreen extends Component {
+  state = {
+    containerOpacity: new Animated.Value(0),
+  };
+
+  componentDidMount() {
+    Animated.timing(this.state.containerOpacity, {
+      toValue: 1,
+      duration: 900,
+    }).start();
+  }
+
   render() {
     const user = firebase.auth().currentUser;
     return (
-      <View style={{ flex: 1 }}>
+      <Animated.View style={{ flex: 1, opacity: this.state.containerOpacity }}>
         <Text> {user.email} </Text>
-        <Image source={{ uri: user.photoURL }} style={{ width: 50, height: 50 }} />
-      </View>
+        <Image source={{ uri: user.photoURL }} style={{ width: 50, height: 50, borderRadius: 50 }} />
+      </Animated.View>
     );
   }
 }
